ef56be630731acb5cb39a94271dfd780c4a2fb2e
ValeriiaGrebneva <grebneva.lera97@gmail.com>
2025-09-10 22:57:15 -0700

    Config is in progress

--- internal/config/config_json.go
+++ internal/config/config_json.go
@@ -1,6 +1,25 @@
 package config
 
+import "os"
+
 type Config struct {
 	Connection string `json:"db_url"`
 	Username   string `json:"current_user_name"`
+}
+
+func Read(path string) (Config, error) {
+	link := os.UserHomeDir + path
+
+	data, err := os.ReadFile(link)
+	if err != nil {
+        return nil, err
+	}
+
+	var configStruct Config
+    decoder := json.NewDecoder(data)
+    if err := decoder.Decode(&configStruct); err != nil {
+        return nil, err
+    }
+
+    return configStruct, nil
 }
