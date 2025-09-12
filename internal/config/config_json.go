41e3437e6b880b3a62d06ca5e626c9a59cc850fb
ValeriiaGrebneva <grebneva.lera97@gmail.com>
2025-09-11 22:52:58 -0700

    Fixing reading from a file

--- internal/config/config_json.go
+++ internal/config/config_json.go
@@ -1,25 +1,53 @@
 package config
 
-import "os"
+import (
+	"encoding/json"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
 
 type Config struct {
 	Connection string `json:"db_url"`
 	Username   string `json:"current_user_name"`
 }
 
-func Read(path string) (Config, error) {
-	link := os.UserHomeDir + path
+func Read() (Config, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, err
+	}
+	link := homeDir + string(configFileName)
 
 	data, err := os.ReadFile(link)
 	if err != nil {
-        return nil, err
+        return Config{}, err
 	}
 
 	var configStruct Config
-    decoder := json.NewDecoder(data)
-    if err := decoder.Decode(&configStruct); err != nil {
-        return nil, err
+
+	if err := json.Unmarshal(data, &configStruct); err != nil {
+        return Config{}, err
     }
+    //decoder := json.NewDecoder(strings.NewReader(data))
+    //if err := decoder.Decode(&configStruct); err != nil {
+    //    return Config{}, err
+    //}
 
     return configStruct, nil
+}
+
+func (configStruct Config) SetUser(current_user_name string) error {
+	configStruct.Username = current_user_name
+	jsonStruct, err := json.Marshal(&configStruct)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(configFileName, jsonStruct, 0666) 
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"github.com/ValeriiaGrebneva/BlogAggregator/internal/config"
+)
+
 func main() {
+	configStruct, err := config.Read()
+	if err != nil {
+		fmt.Println("Reading didn't work")
+	}
+
+	err = configStruct.SetUser("Valeriia")
+	if err != nil {
+		fmt.Println("User wasn't set")
+	}
+
+	configStruct, err = config.Read()
+	if err != nil {
+		fmt.Println("Reading again didn't work")
+	}
+
 	return
 }
